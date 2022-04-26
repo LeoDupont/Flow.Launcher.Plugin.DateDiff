7b6388734882d74b69286d91523601e19909cded
Léo Dupont <leo.dpt@gmail.com>
2022-04-26 22:28:00 +0200

    show clean error if Node is not installed

--- start.bat
+++ start.bat
@@ -1,3 +1,15 @@
 @echo off
 SET plugin_dir=%~dp0%
-node %plugin_dir%/src/main.js %*
+SET icon=%plugin_dir%images\icon.png
+:: JSON doesn't like backslashes so we convert them to forward slashes
+SET "icon=%icon:\=/%"
+SET title="Error: Node.js is not installed on your system."
+SET subtitle="Please install it from nodejs.org to use the DateDiff plugin!"
+
+
+where /q node
+IF ERRORLEVEL 1 (
+    echo {"result": [{"Title": %title%, "Subtitle": %subtitle%, "IcoPath": "%icon%"}]}
+) ELSE (
+    node %plugin_dir%/src/main.js %*
+)
